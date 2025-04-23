af30a1168c16ced0aaaa34ce49d8b3c80c31eeae
flosrv <flosrv123@gmail.com>
2025-04-23 22:00:35 +0200

    � Remove all constants.py files

--- Exito WIP/imports.py
+++ Exito WIP/imports.py
@@ -1,4 +1,5 @@
 from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
